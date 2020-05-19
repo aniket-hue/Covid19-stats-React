2d1e54575467d5f99159b2fc867d1bf25381e1c2
aniket-hue <freakyyo26@gmail.com>
2020-05-20 05:09:39 +0530

    Updates

---
+++ precache-manifest.7b4ce75c8f8b65df9ef5aebc1ad5a513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd778f27b5c2464718b36f8342d395f7",
+    "url": "/Covid19-stats-React/index.html"
+  },
+  {
+    "revision": "1b72054da151c83d6560",
+    "url": "/Covid19-stats-React/static/css/main.52f89dfa.chunk.css"
+  },
+  {
+    "revision": "b9a154faa15ed922fc04",
+    "url": "/Covid19-stats-React/static/js/2.a4c1fe87.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-stats-React/static/js/2.a4c1fe87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b72054da151c83d6560",
+    "url": "/Covid19-stats-React/static/js/main.9d2de8d4.chunk.js"
+  },
+  {
+    "revision": "0830804d2ec47ef72bed",
+    "url": "/Covid19-stats-React/static/js/runtime-main.c04e1fc8.js"
+  },
+  {
+    "revision": "51087c649b277c7a63be396407c51e58",
+    "url": "/Covid19-stats-React/static/media/image.51087c64.png"
+  }
+]);
